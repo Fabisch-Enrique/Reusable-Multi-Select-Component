3f4a4ec8afd0af7c9e7b184041b15f388a43a7d9
Fabisch <drenriquenicol@gmail.com>
2022-12-07 06:45:37 +0300

    send(self) and fast

--- lib/rms_web/live/detail_live.ex
+++ lib/rms_web/live/detail_live.ex
@@ -22,10 +22,9 @@ defmodule RmsWeb.DetailLive do
           <.live_component
             id="multi"
             module={RmsWeb.MultiSelectComponent}
-            occupation={@user.occupation}
+            values={@user.occupation}
             user={@user}
             form={f}
-
             >
           </.live_component>
         </.form>
@@ -33,7 +32,35 @@ defmodule RmsWeb.DetailLive do
     """
   end
 
-  # def handle updated_occupation({:updated_occupation, occupation}, socket) do
+  def handle_info({:update_occupations, selected_occupation_id}, %{assigns: %{user: user}} = socket) do
 
-  # end
+    user_occupations =
+      user.occupation
+      |> Enum.map(fn occ ->
+        occupation_struct = Map.from_struct(occ)
+
+        if occ.id == selected_occupation_id do
+          occupation_struct
+          |> Map.update!(:selected, fn _ -> !occupation_struct.selected end)
+        else
+          occupation_struct
+        end
+      end)
+
+    user
+    |> Ecto.Changeset.change(%{occupation: user_occupations})
+    |> Rms.Repo.update()
+    |> case do
+      {:ok, user} ->
+         socket
+         |> assign(:user, user)
+         |> assign(:changeset, User.changeset(user, %{}))
+
+      _ ->
+        socket
+    end
+
+    {:noreply, socket}
+  end
+
 end
--- lib/rms_web/live/multiselect_component.ex
+++ lib/rms_web/live/multiselect_component.ex
@@ -2,51 +2,25 @@ defmodule RmsWeb.MultiSelectComponent do
   use RmsWeb, :live_component
 
   alias Phoenix.LiveView.JS
-  alias Rms.Users.User
 
   def update(params, socket) do
-    %{user: user, id: id, occupation: occupation, form: form} = params
+    %{id: id, values: selected_listing, form: form} = params
 
     {:ok,
      socket
      |> assign(:id, id)
-     |> assign(:changeset, User.changeset(user, %{}))
      |> assign(:form, form)
-     |> assign(:user, user)
-     |> assign(:selected_occupation, filter_selected_occupation(occupation))}
+     |> assign(:selected_values, filter_selected_value(selected_listing))}
   end
 
-  def handle_event("update_selected", %{"occupation-id" => occupation_id}, %{assigns: %{user: user}} = socket) do
-    user_occupations =
-      user.occupation
-      |> Enum.map(fn occ ->
-        occupation_struct = Map.from_struct(occ)
+  def handle_event("update_selected", %{"occupation-id" => selected_occupation_id}, socket) do
+    send(self(), {:update_occupation, selected_occupation_id})
 
-        if occ.id == occupation_id do
-          occupation_struct
-          |> Map.update!(:selected, fn _ -> !occupation_struct.selected end)
-        else
-          occupation_struct
-        end
-      end)
-
-    user
-    |> Ecto.Changeset.change(%{occupation: user_occupations})
-    |> Rms.Repo.update()
-    |> case do
-      {:ok, user} ->
-        {:noreply,
-         socket
-         |> assign(:user, user)
-         |> assign(:changeset, User.changeset(user, %{}))
-         |> assign(:selected_occupation, filter_selected_occupation(user.occupation))}
-
-      _ ->
-        {:noreply, socket}
-    end
+    {:noreply, socket}
   end
 
-  defp filter_selected_occupation(occupation) do
+  defp filter_selected_value(occupation) do
     Enum.filter(occupation, fn occ -> occ.selected == true end)
   end
+
 end
